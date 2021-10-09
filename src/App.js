e7de29d423bc25d4b4786cdb468dce253fd1cc54
joemeatball <39838100+joemeatball@users.noreply.github.com>
2021-10-09 16:10:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 function App() {
   return (
 <script src="https://apis.google.com/js/platform.js" async defer></script>
-   
+   <meta name="google-signin-client_id" content="550364092484-bfg270ovnshopajsptqcgglrm2dfhevc.apps.googleusercontent.com.apps.googleusercontent.com">
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
     </div>
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
   );
 }
 
