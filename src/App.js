0919a571f3f18a96d7a4b4cba59fdcad00e1cadb
joemeatball <39838100+joemeatball@users.noreply.github.com>
2021-10-09 16:18:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-   <meta name="google-signin-client_id" content="550364092484-bfg270ovnshopajsptqcgglrm2dfhevc.apps.googleusercontent.com.apps.googleusercontent.com">
-    <div className="App">
+ <div className="App">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+   <meta name="google-signin-client_id" content="550364092484-bfg270ovnshopajsptqcgglrm2dfhevc.apps.googleusercontent.com.apps.googleusercontent.com"/>
+   
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
